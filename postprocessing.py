982a7f1df3c3e6cf1fb9de782931c5ca2e23280a
vtagle <vtagle@gmail.com>
2022-03-23 16:33:34 -0700

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,10 +4,10 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+ghPagesURL = "https://vtagle.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+# ghPagesURL = "https://tiktokrss.conoroneill.com/"
 
 api = TikTokApi()
 
